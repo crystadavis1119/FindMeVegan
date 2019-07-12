28cbb6137cd9c19237eefaeda7c9f4f68300abea
crystadavis1119 <crystadavis1119@hotmail.com>
2019-07-12 11:40:52 -0500

    working on jwt auth

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import Main from '../../Main'
 import SignupPage from '../SignupPage/SignupPage';
 import LoginPage from '../LoginPage/LoginPage'
-// import userService from '../../utils/userService';
+import userService from '../../utils/userService';
 
 
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: false,
+            user: userService.getUser(),
+        }
+    }
+
+    handleLogout = () => {
+        userService.logout();
+        this.setState({ user: null });
+    }
+
+    handleSignupOrLogin = () => {
+        this.setState({user: userService.getUser()});
+    }
 
 
-export default function App () {
-    return(
-        <div>
-            <Router>
-                <Switch>
-                    <Route exact path="/" render={() => <LoginPage />} />
-                    <Route path="/signup" render={() => <SignupPage />} />
-
-                    <Main />
-                </Switch>
-            </Router>
-
-        </div>
-    )
+    render() {
+        return(
+            <div>
+                <Router>
+                    <Switch>
+                        {/* <Route exact path="/" render={() => <LoginPage />} /> */}
+                        <Route path="/" render={() => <SignupPage />} />
+                        <Main />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
