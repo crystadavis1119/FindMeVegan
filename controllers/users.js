fd2ea617e17a0436ffb4e723af8c4bbeb8dc6ab2
crystadavis1119 <crystadavis1119@hotmail.com>
2019-07-15 12:41:46 -0500

    login functionality

--- controllers/users.js
+++ controllers/users.js
@@ -1,33 +1,49 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-
 const SECRET = process.env.SECRET;
 
 module.exports = {
-  signup
+  signup,
+  login,
+  createJWT
 };
 
 async function signup(req, res) {
-    const user = new User(req.body);
-    console.log('FOOOOOOO', user);
-    try {
-      console.log('before');
-      await user.save();
-      console.log('after')
-      const token = createJWT(user);
-      console.log(token);
-      res.json({ token });
-    } catch (err) {
-      console.log(err);
-      // Probably a duplicate email
-      res.status(400).json(err);
-    }
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (err) {
+    // Probably a duplicate email
+    res.status(400).json(err);
+  }
 }
 
+async function login(req, res) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(401).json({err: 'bad credentials'});
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({token});
+      } else {
+        console.log(req.body)
+        return res.status(401).json({err: 'bad credentials'});
+      }
+    });
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+}
+
+/*----- Helper Functions -----*/
+
 function createJWT(user) {
-    return jwt.sign(
-      {user}, // data payload
-      SECRET,
-      {expiresIn: '24h'}
-    );
+  return jwt.sign(
+    {user}, // data payload
+    SECRET,
+    {expiresIn: '24h'}
+  );
 }
--- models/user.js
+++ models/user.js
@@ -20,16 +20,24 @@ userSchema.set('toJSON', {
 });
 
 userSchema.pre('save', function(next) {
-    // this will be set to the current document
-    const user = this;
-    if (!user.isModified('password')) return next();
-    // password has been changed - salt and hash it
-    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
-        if (err) return next(err);
-        // replace the user provided password with the hash
-        user.password = hash;
-        next();
-    });
+  // 'this' will be set to the current document
+  const user = this;
+  if (!user.isModified('password')) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
 });
+
+userSchema.methods.comparePassword = function(tryPassword, cb) {
+  // 'this' represents the document that you called comparePassword on
+  bcrypt.compare(tryPassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
