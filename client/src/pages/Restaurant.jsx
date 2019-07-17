a58d66e7762275164cbcfddedc9a96df5ad30e72
crystadavis1119 <crystadavis1119@hotmail.com>
2019-07-17 01:14:47 -0500

    fixed hours/days

--- client/src/pages/Restaurant.jsx
+++ client/src/pages/Restaurant.jsx
@@ -55,20 +55,20 @@ class Restaurant extends Component {
   
 
   dayMap = {
-    1: 'Mon',
-    2: 'Tue',
-    3: 'Wed',
-    4: 'Thu',
-    5: 'Fri',
-    6: 'Sat',
-    7: 'Sun'
+    0: 'Mon',
+    1: 'Tue',
+    2: 'Wed',
+    3: 'Thu',
+    4: 'Fri',
+    5: 'Sat',
+    6: 'Sun'
   }
 
   render() {
     const { restaurant } = this.state;
     const { classes } = this.props;
 
-    if (!restaurant) return <div>Loading</div>;
+    if (!restaurant) return <div> Loading... </div>;
     console.log(restaurant);
     
     const regularHours = restaurant.hours[0].open;
