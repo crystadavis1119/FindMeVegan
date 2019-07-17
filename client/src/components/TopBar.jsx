87bfe9f3619849ee048895d53c02dcd5566999c5
crystadavis1119 <crystadavis1119@hotmail.com>
2019-07-16 22:23:09 -0500

    individual restaurant info

--- client/src/components/TopBar.jsx
+++ client/src/components/TopBar.jsx
@@ -4,12 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+// import MenuIcon from '@material-ui/icons/Menu';
 // import Switch from '@material-ui/core/Switch';
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import FormGroup from '@material-ui/core/FormGroup';
@@ -43,9 +43,9 @@ export default function TopBar({ auth, handleLogout }) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" className={classes.title}>
             Find Me Vegan
           </Typography>
--- client/src/pages/Restaurant.jsx
+++ client/src/pages/Restaurant.jsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '8%'
+  },
+  title: {
+    fontSize: 20,
+    margin: 15,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  media: {
+    height: 140,
+  },
+  root: {
+    width: '80%',
+    margin: '0 auto',
+    marginBottom: '10%'
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+});
 
 
 class Restaurant extends Component {
@@ -21,14 +54,82 @@ class Restaurant extends Component {
 
   render() {
     const { restaurant } = this.state;
+    const { classes } = this.props;
 
     if (!restaurant) return <div>Loading</div>;
     console.log(restaurant);
 
+
     return (
-      <div>{restaurant.name}</div>
+      <div>
+        <div className={classes.container}>
+          <CardContent>
+
+            <CardMedia
+              className={classes.media}
+              image={restaurant.image_url}
+            />
+            <br />
+
+            <Typography variant="h3" component="h2">
+              {restaurant.name}
+            </Typography>
+
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              {restaurant.location.display_address}
+            </Typography>
+            <hr />
+
+          </CardContent>
+        </div>
+        <div className={classes.root}>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>Contact Information</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                {restaurant.display_phone}
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>Hours</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                {restaurant.hours.open}
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>More Restaurant Info</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                <a href={restaurant.url}>Visit {restaurant.name} on Yelp here</a>
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+
+        </div>
+      </div>
     )
   }
 }
 
-export default withRouter(Restaurant)
+export default withRouter(withStyles(styles)(Restaurant))
